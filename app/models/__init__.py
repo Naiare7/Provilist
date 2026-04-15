f9a7e6533d893b0e9e896ddc03c25ee8be632e17
Kevin Ruiz <kevingerardoruiz.2001@gmail.com>
2026-04-15 22:13:54 +0200

    preparando nuevo modelo para ejecutar migracion

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,7 +1,8 @@
 from .provincia import Provincia
 from .capital import Capital
 from .municipio import Municipio
+from .user import User
 
 # Esto facilita la importación en otros archivos de la app:
-# from app.models import Provincia, Capital, Municipio
-__all__ = ["Provincia", "Capital", "Municipio"]
+# from app.models import Provincia, Capital, Municipio, User
+__all__ = ["Provincia", "Capital", "Municipio", "User"]
